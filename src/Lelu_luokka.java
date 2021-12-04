9bdcc00598a50582e7c6e57079e304fb330af4c0
hsaros81 <90245317+hsaros81@users.noreply.github.com>
2021-12-04 10:58:23 +0200

    Pohja
    Lelu-luokan pohja ja ensimmäinen tehtävä

--- src/Lelu_luokka.java
+++ src/Lelu_luokka.java
@@ -1,9 +1,28 @@
 
-public class Lelu_luokka {
+public class Lelu_luokka 
+{
 
 	public static void main(String[] args) 
 	{
 
 	}
 
+} //Main-lopetus
+
+class Lelu
+{
+	public String nimi;
+	public int paino;
+	
+	public Lelu(String nimi, int paino) //muodostin
+	{
+		this.nimi = nimi;
+		this.paino = paino;
+	}
+	
+	public void tulosta() //tulostin
+	{
+		System.out.println("Lelun nimi on: " + nimi);
+		System.out.println("Lelun paino on: " + paino);
+	}
 }
