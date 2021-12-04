db4c40f1c4f9a1c6ad3f0ea684903679102e3b83
hsaros81 <90245317+hsaros81@users.noreply.github.com>
2021-12-04 11:40:01 +0200

    Lopullinen ohjelma
    Valmis ohjelma

--- src/Lelu_luokka.java
+++ src/Lelu_luokka.java
@@ -4,7 +4,12 @@ public class Lelu_luokka
 
 	public static void main(String[] args) 
 	{
-
+		Lelu lelu1 = new Lelu("Palikka", 13);
+		lelu1.tulostaTiedot();
+		System.out.println("");
+		
+		Vieteriauto vieteriauto1 = new Vieteriauto("Puuauto", 40, 1);
+		vieteriauto1.tulostaTiedot();
 	}
 
 } //Main-lopetus
@@ -12,7 +17,7 @@ public class Lelu_luokka
 class Lelu
 {
 	public String nimi;
-	public int paino;
+	protected int paino;
 	
 	public Lelu(String nimi, int paino) //muodostin
 	{
@@ -20,9 +25,33 @@ class Lelu
 		this.paino = paino;
 	}
 	
-	public void tulosta() //tulostin
+	protected void tulostaTiedot() //tulostin
 	{
 		System.out.println("Lelun nimi on: " + nimi);
 		System.out.println("Lelun paino on: " + paino);
+	}
+} //Lelu-luokan lopetus
+
+class Vieteriauto extends Lelu
+{
+	public int nopeus;
+
+	public Vieteriauto(String nimi, int paino, int nopeus)
+	{
+		super(nimi, paino);
+		this.nopeus = nopeus;
+	}
+		
+	public void kiihdyta()
+	{
+		nopeus++;
+	}
+	
+	@Override
+	public void tulostaTiedot()
+	{
+		System.out.println("Lelun nimi on: " + nimi);
+		System.out.println("Lelun paino on: " + paino);
+		System.out.println("Lelun nopeus on: " + nopeus);
 	}
 }
