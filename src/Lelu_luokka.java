edbdc807e9de28fb7f793d57f72e6cf19661840e
hsaros81 <90245317+hsaros81@users.noreply.github.com>
2021-12-04 11:43:55 +0200

    Viimeistely
    Lisäsin kommentit koodiin

--- src/Lelu_luokka.java
+++ src/Lelu_luokka.java
@@ -4,20 +4,20 @@ public class Lelu_luokka
 
 	public static void main(String[] args) 
 	{
-		Lelu lelu1 = new Lelu("Palikka", 13);
-		lelu1.tulostaTiedot();
+		Lelu lelu1 = new Lelu("Palikka", 13); //Luodaan olio lelu1
+		lelu1.tulostaTiedot(); //Tulostetaan olion tiedot
 		System.out.println("");
 		
-		Vieteriauto vieteriauto1 = new Vieteriauto("Puuauto", 40, 1);
-		vieteriauto1.tulostaTiedot();
+		Vieteriauto vieteriauto1 = new Vieteriauto("Puuauto", 40, 1); //Luodaan olio vieteriauto1
+		vieteriauto1.tulostaTiedot(); //Tulostetaan uuden olion tiedot
 	}
 
 } //Main-lopetus
 
-class Lelu
+class Lelu //Luokka Lelu
 {
-	public String nimi;
-	protected int paino;
+	public String nimi; //muuttuja nimi
+	protected int paino; //muuttuja paino
 	
 	public Lelu(String nimi, int paino) //muodostin
 	{
@@ -25,30 +25,30 @@ class Lelu
 		this.paino = paino;
 	}
 	
-	protected void tulostaTiedot() //tulostin
+	protected void tulostaTiedot() //Metodi tietojen tulostukseen
 	{
 		System.out.println("Lelun nimi on: " + nimi);
 		System.out.println("Lelun paino on: " + paino);
 	}
 } //Lelu-luokan lopetus
 
-class Vieteriauto extends Lelu
+class Vieteriauto extends Lelu //Luokka Vieteriauto, joka perii Lelu-luokan tiedot
 {
-	public int nopeus;
+	public int nopeus; //Uusi muuttuja nopeus
 
-	public Vieteriauto(String nimi, int paino, int nopeus)
+	public Vieteriauto(String nimi, int paino, int nopeus) //Muodostin vieteriautolle
 	{
 		super(nimi, paino);
 		this.nopeus = nopeus;
 	}
 		
-	public void kiihdyta()
+	public void kiihdyta() //Metodi kiihdyta
 	{
 		nopeus++;
 	}
 	
 	@Override
-	public void tulostaTiedot()
+	public void tulostaTiedot() //Metodi, joka tulostaa tiedot
 	{
 		System.out.println("Lelun nimi on: " + nimi);
 		System.out.println("Lelun paino on: " + paino);
